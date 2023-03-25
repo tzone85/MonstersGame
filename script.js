eeada2f8db6f0e628ff21b2a3c87b30656b02f45
Thando Mini <tzone85@gmail.com>
2023-03-25 17:04:16 +0200

    draw the dummified player and started initiating basic settings like x y and radius collisions. made use of canvas properties like globalAlpha to apply only to the player

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 window.addEventListener('load', function (){
     const canvas = document.getElementById('canvas1');
 
-    const context = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
     canvas.width = 1280;
     canvas.height = 720;
+
+    // place here to avoid updating state with every draw
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'while';
 
     class Player {
         constructor(game) {
             this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 30;
+        }
+
+        draw(context) {
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
         }
     }
 
@@ -18,9 +36,14 @@ window.addEventListener('load', function (){
             this.height = this.canvas.height;
             this.player = new Player(this);
         }
+
+        render(context) {
+            this.player.draw(context);
+        }
     }
 
     const game = new Game(canvas);
+    game.render(ctx);
     console.log(game);
 
     function animate() {
