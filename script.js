7f2d89ec8f14d6c13ce8258bb565b667d98eab7a
Thando Mini <tzone85@gmail.com>
2023-03-25 16:32:36 +0200

    added some js oop classes

--- script.js
+++ script.js
@@ -5,5 +5,26 @@ window.addEventListener('load', function (){
     canvas.width = 1280;
     canvas.height = 720;
 
+    class Player {
+        constructor(game) {
+            this.game = game;
+        }
+    }
+
+    class Game {
+        constructor(canvas) {
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.player = new Player(this);
+        }
+    }
+
+    const game = new Game(canvas);
+    console.log(game);
+
+    function animate() {
+
+    }
 });
 
